614c21e1c4b15a4a7e58449afca50170966e0560
NanaBuabeng <89571060+NanaBuabeng@users.noreply.github.com>
2023-11-08 17:50:09 +0100

    Model binding and filtering

--- WebApp2/ClientApp/src/app/Flashcards/flashcards.component.ts
+++ WebApp2/ClientApp/src/app/Flashcards/flashcards.component.ts
@@ -9,6 +9,18 @@ import { IFlashcard } from './flashcard';
 export class FlashcardsComponent {
   viewTitle: string = 'Table'
   displayImage: boolean = true;
+  // listFilter: string = '';
+
+  private _listFilter: string = '';
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log('In setter:', value)
+    this.filteredFlashcards = this.performFilter(value);
+  }
+
 
   flashcards: IFlashcard[] = [
     {
@@ -23,12 +35,25 @@ export class FlashcardsComponent {
       "Name": "Bil",
       "Price": 20,
       "Description": "Car",
-      "ImageUrl": "assets/images/random.jpg" //Missing image icon shows when an image is linked with no matching image
+      "ImageUrl": "assets/images/test.jpg"
+     //Missing image icon shows when an image is linked with no matching image
     }
   ];
+  filteredFlashcards: IFlashcard[] = this.flashcards;
+
+  performFilter(filterBy: string): IFlashcard[] {
+    filterBy = filterBy.toLowerCase();
+    return this.flashcards.filter((flashcards: IFlashcard) =>
+      flashcards.Name.toLowerCase().includes(filterBy));
+  }
 
   toggleImage(): void {
     this.displayImage = !this.displayImage;
   }
+
+  ngOnInit(): void {
+    console.log('ItemsConponent created');
+  }
+
 
 }
