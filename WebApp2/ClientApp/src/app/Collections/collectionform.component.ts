fdc2b0740522fa8263966388914c28a5ccc86a63
William Promnoi Evensen <s364581@oslomet.no>
2023-11-24 20:23:30 +0100

    Somewhat working Collection Creation

--- WebApp2/ClientApp/src/app/Collections/collectionform.component.ts
+++ WebApp2/ClientApp/src/app/Collections/collectionform.component.ts
@@ -1,9 +1,10 @@
 import { Component } from "@angular/core";
-import { FormGroup, FormControl, Validators, FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { ActivatedRoute, Router } from "@angular/router";
 import { CollectionService } from './collections.service';
 import { IFlashcard } from '../Flashcards/flashcard';
 import { FlashcardService } from '../Flashcards/flashcards.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: "app-collections-collectionform",
@@ -15,70 +16,59 @@ export class CollectionformComponent {
   isEditMode: boolean = false;
   collectionId: number = -1;
   flashcards: IFlashcard[] = [];
-  selected: IFlashcard[] = [];
 
-
-  constructor(private _collectionService: CollectionService, private _flashcardService: FlashcardService, private _formbuilder: FormBuilder, private _router: Router, private _route: ActivatedRoute) {
+  constructor(
+    private _collectionService: CollectionService,
+    private _flashcardService: FlashcardService,
+    private _formbuilder: FormBuilder,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {
     this.collectionForm = _formbuilder.group({
       collectionName: ['', Validators.required],
-      collectionDate: [''],
-      totalFlashcards: [''],
-      flashcards: [''],
-      //collectionFlashcard: [''],
-      //contributerId: [''],
-      //contributers: [''],
+      flashcards: [[]],
     });
 
     // Automatic data insertion
-    this.collectionForm.patchValue({
-      collectionDate: new Date().getDate() + "." + (new Date().getMonth() + 1) + "." + new Date().getFullYear(),
-      totalFlashcards: this.selected.length,
-      flashcards: this._flashcardService.getFlashcards().subscribe(data => {
-        console.log('All', data);
-        this.flashcards = data;
-        console.log("selected flashcards:", this.selected);
-      }),
-
-    })
-  }
-
-  onChange(value: any) {
-    console.log("OnChange value: ", value);
+    forkJoin([
+      this._flashcardService.getFlashcards(),
+    ]).subscribe(
+      ([flashcards]) => {
+        this.flashcards = flashcards;
+        this.collectionForm.patchValue({
+          collectionDate: new Date().getDate() + "." + (new Date().getMonth() + 1) + "." + new Date().getFullYear(),
+          flashcards: [],
+        });
+      },
+      (error) => {
+        console.error('Error fetching data:', error);
+      }
+    );
   }
 
   onSubmit() {
     console.log("CollectionCreate form submitted:");
-    console.log("CollectionForm" + this.collectionForm);
     console.log("The collection " + this.collectionForm.value.collectionName + " is created.");
     console.log("Touched? " + this.collectionForm.touched);
-    console.log("Selected" + this.selected)
+    console.log("Selected", this.collectionForm.value.flashcards);
+
     const newCollection = this.collectionForm.value;
-
-
 
     if (this.isEditMode) {
       this._collectionService.updateCollection(this.collectionId, newCollection)
-        .subscribe(response => {
-          if (response.success) {
-            console.log(response.message);
-            this._router.navigate(['/collections']);
-          }
-          else {
-            console.log('Collection update failed');
-          }
-        });
-    }
-    else {
+        .subscribe(response => this.handleResponse(response));
+    } else {
       this._collectionService.createCollection(newCollection)
-        .subscribe(response => {
-          if (response.success) {
-            console.log(response.message);
-            this._router.navigate(['/collections']);
-          }
-          else {
-            console.log('Collections creation failed');
-          }
-        });
+        .subscribe(response => this.handleResponse(response));
+    }
+  }
+
+  private handleResponse(response: any) {
+    if (response.success) {
+      console.log(response.message);
+      this._router.navigate(['/collections']);
+    } else {
+      console.log('Collection operation failed');
     }
   }
 
@@ -99,15 +89,13 @@ export class CollectionformComponent {
   }
 
   loadCollectionForEdit(collectionId: number) {
-    this._collectionService.getCollectionById(this.collectionId)
+    this._collectionService.getCollectionById(collectionId)
       .subscribe(
         (collection: any) => {
-          console.log('retrived collection: ', collection);
+          console.log('retrieved collection: ', collection);
           this.collectionForm.patchValue({
             collectionName: collection.collectionName,
-            collectionDate: collection.collectionDate,
-            contributer: collection.contributer,
-            totalFlashcards: collection.totalFlashcards
+            flashcards: collection.flashcards, // Assuming 'flashcards' is the property name in your collection model
           });
         },
         (error: any) => {
