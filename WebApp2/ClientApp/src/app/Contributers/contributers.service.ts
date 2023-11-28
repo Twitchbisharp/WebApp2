d8ccf61a8d93bcdf31c613ec02ce3501091b995c
William Promnoi Evensen <s364581@oslomet.no>
2023-11-28 20:06:49 +0100

    Contributer CollectionName retrieval and output.

--- WebApp2/ClientApp/src/app/Contributers/contributers.service.ts
+++ WebApp2/ClientApp/src/app/Contributers/contributers.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { IContributer } from "./contributer";
+import { ICollection } from '../Collections/collection';
 
 @Injectable({
   providedIn: "root"
@@ -14,6 +15,10 @@ export class ContributerService {
 
   getContributers(): Observable<IContributer[]> {
     return this._http.get<IContributer[]>(this.baseUrl);
+  }
+
+  getCollections(): Observable<ICollection[]> {
+    return this._http.get<ICollection[]>(this.baseUrl);
   }
 
   createContributer(newContributer: IContributer): Observable<any> {
