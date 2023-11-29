043463aac97c7d56d377735ac267e07fa82337e4
NanaBuabeng <s39009@osloskolen.no>
2023-11-29 15:45:09 +0100

    unit testing

---
+++ xUnitTestWebapp2/Controllers/CollectionControllerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using WebApp2.Controllers;
+using WebApp2.DAL;
+using WebApp2.Models;
+
+
+namespace xUnitTestWebapp2.Controllers
+{
+    public class CollectionControllerTest
+    {
+        [Fact]
+        public async Task TestGetAll()
+        {
+            // Arrange
+            var collectionList = new[]
+            {
+                new Collection
+                {
+                    CollectionId = 1,
+                    CollectionName = "City",
+                   
+                    
+                },
+                new Collection
+                {
+                    CollectionId = 2,
+                    CollectionName = "Foods",
+                   
+                }
+            };
+            var mockCollectionRepository = new Mock<ICollectionRepository>();
+            mockCollectionRepository.Setup(repo => repo.GetAll()).ReturnsAsync(collectionList);
+            var mockLogger = new Mock<ILogger<CollectionController>>();
+            var collectionController = new CollectionController(mockCollectionRepository.Object, mockLogger.Object);
+
+            // Act
+            var result = await collectionController.GetAll();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<Collection[]>(okResult.Value);
+            Assert.Equal(collectionList, model);
+        }
+
+        [Fact]
+        public async Task TestCreateNotOk()
+        {
+            // Arrange
+            var testCollection = new Collection
+            {
+                CollectionId = 1,
+                CollectionName = "Cakes",
+                
+            };
+            var mockCollectionRepository = new Mock<ICollectionRepository>();
+            mockCollectionRepository.Setup(repo => repo.Create(testCollection)).ReturnsAsync(false);
+            var mockLogger = new Mock<ILogger<CollectionController>>();
+            var collectionController = new CollectionController(mockCollectionRepository.Object, mockLogger.Object);
+
+            // Act
+            var result = await collectionController.Create(testCollection);
+
+            // Assert
+            var viewResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsAssignableFrom<object>(viewResult.Value);
+           
+        }
+
+    }
+}
---
+++ xUnitTestWebapp2/Controllers/ContributerControllerTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApp2.Controllers;
+using WebApp2.DAL;
+using WebApp2.Models;
+
+namespace xUnitTestWebapp2.Controllers
+{
+    public class ContributerControllerTests
+    {
+        [Fact]
+        public async Task TestGetAll()
+        {
+            // Arrange
+            var contributerList = new[]
+            {
+                new Contributer
+                {
+                    ContributerId = 1,
+                    Name = "AJ",
+                    
+                },
+                new Contributer
+                {
+                    ContributerId = 2,
+                    Name = "Mickey",
+                    
+                }
+            };
+            var mockContributerRepository = new Mock<IContributerRepository>();
+            mockContributerRepository.Setup(repo => repo.GetAll()).ReturnsAsync(contributerList);
+            var mockLogger = new Mock<ILogger<ContributerController>>();
+            var contributerController = new ContributerController(mockContributerRepository.Object, mockLogger.Object);
+
+            // Act
+            var result = await contributerController.GetAll();
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            var model = Assert.IsAssignableFrom<Contributer[]>(okResult.Value);
+            Assert.Equal(contributerList, model);
+        }
+
+        [Fact]
+        public async Task TestCreateNotOk()
+        {
+            // Arrange
+            var testContributer = new Contributer
+            {
+                ContributerId = 1,
+                Name = "Lina",
+            };
+            var mockContributerRepository = new Mock<IContributerRepository>();
+            mockContributerRepository.Setup(repo => repo.Create(testContributer)).ReturnsAsync(false);
+            var mockLogger = new Mock<ILogger<ContributerController>>();
+            var contributerController = new ContributerController(mockContributerRepository.Object, mockLogger.Object);
+
+            // Act
+            var result = await contributerController.Create(testContributer);
+
+            // Assert
+            var viewResult = Assert.IsType<OkObjectResult>(result);
+            var response = Assert.IsAssignableFrom<object>(viewResult.Value);
+            
+        }
+
+    }
+}
